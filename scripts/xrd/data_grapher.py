f45f8527155dbaf62e1798de9e90145d9c33551b
matusmoro <matus.moro@gmail.com>
2025-08-20 11:47:28 +0200

    Proper graph output pathing.

--- scripts/xrd/data_grapher.py
+++ scripts/xrd/data_grapher.py
@@ -41,11 +41,11 @@ class Data:
         ax.scatter(self.angle[self.max_index], self.max_height, c='r')
         ax.plot(self.angle, self.intensity)
         
-        plt.savefig(f'r/home/matusmoro/school/exp_met_2_xrd/graphs/{filename}.png')
+        plt.savefig(f'/home/matusmoro/school/exp_met_2_xrd/graphs/xrd{filename}.png')
 
 if __name__ == "__main__":
-    fast = Data(r'/home/matusmoro/school/exp_met_2_xrd/data/fast_sul.ras', 'fast')
-    slow = Data(r'/home/matusmoro/school/exp_met_2_xrd/data/slow_sul.ras', 'slow')
+    fast = Data(r'/home/matusmoro/school/exp_met_2_xrd/data/xrd/fast_sul.ras', 'fast')
+    slow = Data(r'/home/matusmoro/school/exp_met_2_xrd/data/xrd/slow_sul.ras', 'slow')
 
     fast.graph('fast_test')
     slow.graph('slow_test')
